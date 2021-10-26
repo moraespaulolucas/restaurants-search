a34673fccd9a2ef310d0cd340635000e00b792d6
Paulo Lucas Moraes <moraes.paulolucas@gmail.com>
2021-10-26 15:58:33 -0300

    logo e campo de pesquisa feitos, div para o mapa feita

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField, { Input } from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
 
-import { Container } from './styles'
+import logo from '../../assets/logo.svg';
+
+import { Wrapper, Container, Search, Logo, Map } from './styles'
 
 const Home = () => {
+    const [inputValue, setInputValue] = useState();
     return (
-        <Container>
-            Hello world
-        </Container>
+        <Wrapper>
+            <Container>
+                <Search>
+                    <Logo src={logo} alt="Logo do restaurante" />
+                    <TextField
+                        label='Pesquisar restaurante'
+                        outlined
+                        trailingIcon={<MaterialIcon role="button" icon="search" />}
+                    ><Input
+                            value={inputValue}
+                            onChange={(e) => setInputValue(e.target.value)} />
+                    </TextField>
+                </Search>
+            </Container>
+            <Map />
+        </Wrapper>
     )
 }
 
