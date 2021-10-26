a9cb9e12158b66bc014545ebdfd58d9278ebe73a
Paulo Lucas Moraes <moraes.paulolucas@gmail.com>
2021-10-26 14:53:22 -0300

    reset de estilos e criacao do theme

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles'
+
+const Home = () => {
+    return (
+        <Container>
+            Hello world
+        </Container>
+    )
+}
+
+export default Home;
